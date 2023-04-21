59b00016a89ef6bcfebced819f62526f9b1160f5
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-04-21 21:20:22 +0200

    Ajout requete afin de rechercher un EVENT

--- database/DAO.js
+++ database/DAO.js
@@ -1,5 +1,7 @@
 import { User, Event } from "./models/index.js";
+import { Op } from "sequelize";
 
+//INFO: user.createEvent() , au lieu de passer par son id, on passe par son objet, c'est + ORM friendly
 class DAO {
 
 	constructor(sequelize) {
@@ -51,6 +53,14 @@ async  get_user_by_username(username) {
   });
 }
 
+//get d'un event par id
+async get_user_by_id(id) {
+	return this.sequelize.transaction(async t => {
+	  return await User.findOne({
+		where: { id: id }
+	  });
+	});
+  }
 
 //ajout d'un event
 async  save_event(event) {
@@ -82,34 +92,262 @@ async  update_event_by_name(name, new_name, category, address, description, imag
 	
 	
 	// get d'un event par nom 
+	async  get_events_by_name_like(name) {
+		return Event.findAll({
+			where: {
+				name: {
+					[Op.like]: `%${name.toLowerCase()}%`,
+				  },
+			}
+		});
+	}
+
+	// get d'un event par nom 
 	async  get_event_by_name(name) {
 		return Event.findOne({
 			where: {name: name}
 		});
 	}
 	
-	// utilsier des booléen pour le eager loading, choisi par le caller
-// get d'un event avec ses participants
-// get d'un event avec ses candidats
-// get event avec tout
-
-//get d'un event par id
 //get d'un event par note exact de son organisateur
+async get_events_by_organizer_score(score) {
+	return this.sequelize.transaction( t => {
+	  return Event.findAll({
+		include: [
+		  {
+			model: User,
+			as: 'organizer',
+			where: { score_host: score }
+		  }
+		]
+	  });
+	});
+  }
+
+
+
+
 //get d'un event par note min de son organisateur
+async get_events_by_organizer_score_min(score_min) {
+	return this.sequelize.transaction( t => {
+	  return Event.findAll({
+		include: [{
+		  model: User,
+		  as: 'organizer',
+		  where: { score_host: { [Op.gte]: score_min } }
+		}]
+	  });
+	});
+  }
+
+
 //get d'events par catégorie
-//get d'events par places restantes
+async get_events_by_category(category) {
+	let where = {};
+	if (category!==undefined) where.category = category;
+    return this.sequelize.transaction( t => {
+        return Event.findAll({
+            where: where,
+        });
+    });
+}
 
-// get des camarades d'un utilisateur pour un event donné
 
-// get d'un user par id
-// get d'un utilisateur avec ses events organisés
-// get d'un utilisateur avec ses particpations
-// get d'un utilisateur avec ses candidatures
-// get d'un utilisateur avec tout
+// get all the event
+async get_all_events() {
+	return this.sequelize.transaction(t=> {
+	  return Event.findAll();
+	});
+  }
 
-// user.createEvent() , au lieu de passer par son id, on passe par son objet, c'est + ORM friendly
 
 // get all the users
+async get_all_users() {
+	return this.sequelize.transaction( t => {
+	  return User.findAll();
+	});
+  }
+
+// get d'un utilisateur par id
+async get_user_by_id(id) {
+	return this.sequelize.transaction(t => {
+	  return User.findOne({where: { id: id }});
+	});
+  }
+
+  
+
+// get user avec ses event associés
+  async get_user_with_related_events(id, include_organizedEvents = false, include_candidateEvents = false, include_participantEvents = false) {
+	const include = [];
+	if (include_organizedEvents) include.push({ model: Event, as: 'organizedEvents' });
+	if (include_candidateEvents) include.push({ model: Event, as: 'candidateEvents', through: {attributes: []} });
+	if (include_participantEvents) include.push({ model: Event, as: 'participantEvents', through: {attributes: []} });
+  
+	return this.sequelize.transaction(async t => {
+	  return await User.findOne({
+		where: { id: id },
+		include: include
+	  });
+	});
+  }
+ 
+
+
+  // utilsier des booléen pour le eager loading, choisi par le caller
+  // get d'un event avec organisateur
+  // get d'un event avec ses participants
+  // get d'un event avec ses candidats
+  // get event avec tout
+  async get_event_with_related_users(eventId, include_organizer = false, include_candidates = false, include_participants = false) {
+	const include = [];
+	if(include_organizer) include.push({ model: User, as: 'organizer' });
+	if(include_candidates) include.push({ model: User, as: 'candidates' });
+	if(include_participants) include.push({ model: User, as: 'participants' });
+	return Event.findOne({
+	  where: { id: eventId },
+	  include: include
+	});
+  }
+
+
+//faire candidater
+async apply(userId, eventId) {
+	return this.sequelize.transaction( async t => {
+		const user = await User.findByPk(userId);
+		const event = await Event.findByPk(eventId);
+		if (!user || !event) { throw new Error("User or event not found"); }
+		return event.addCandidate(user);
+	});
+  }
+
+
+//faire candidater avec name
+async apply_by_name(username, event_name) {
+	return this.sequelize.transaction( async t => {
+		const user = await User.findOne( {where: {username: user_username}} );
+		const event = await Event.findOne( {where: {name: event_name}} );
+		if (!user || !event) throw new Error("User or event not found"); 
+		return event.addCandidate(user);
+	});
+}
+
+
+//faire participer
+async participate(userId, eventId) {
+	return this.sequelize.transaction(async t => {
+		const user = await User.findByPk(userId);
+		const event = await Event.findByPk(eventId);
+		if (!user || !event) throw new Error("User or event not found");
+		return event.addParticipant(user);
+	});
+}
+
+  
+//faire participer avec name
+async participate_by_name(username, event_name) {
+	return this.sequelize.transaction(async t => {
+		const user = await User.findOne({ where: { username: username } });
+		const event = await Event.findOne({ where: { name: event_name } });
+		if (!user || !event) throw new Error("User or event not found");
+		return event.addParticipant(user);
+	});
+}
+
+
+
+
+//get d'events par places restantes, catégorie, lieu, date, etc
+async get_events_by_filters({nb_places_wanted, category, range_places, address, description, event_name, username, range_date, score_host_min}) {
+	let info = {
+		attributes: [
+			'id',
+			'name',
+			'participants_number',
+			'category',
+			'address',
+			'description',
+			'image_url',
+			'organizerId',
+			'date',
+			[this.sequelize.literal('participants_number - COUNT(UserId)'), 'nb_places_left'],
+		],
+		include: [
+			{
+				model: User,
+				as: 'participants',
+				through: {attributes: ['UserId']},
+				attributes: [],
+			},
+			{
+				model: User,
+				as: 'organizer',
+				attributes: ['score_host', 'score_participant', 'username', 'email']
+			},
+		],
+		group: ['Event.id'],
+		having: {
+			[Op.and]: [
+			//   this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('category')), Op.like, `%${category.toLowerCase()}%`),
+			//   { participants_number: {[Op.between]: [range_places.min, range_places.max]} },
+			//   this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('description')), Op.like, `%${description.toLowerCase()}%`),
+			//   this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('name')), Op.like, `%${event_name.toLowerCase()}%`),
+			//   this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('address')), Op.like, `%${address.toLowerCase()}%`),
+			//   this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('organizer.username')), Op.like, `%${username.toLowerCase()}%`),
+			//   { 'nb_places_left': {[Op.gte]: nb_places_wanted} },
+			//   { date : {[Op.lte]: range_date.max, [Op.gte]: range_date.min} },
+			//   { 'organizer.score_host': {[Op.or]: {[Op.gte]: score_host_min, [Op.eq]: null} } }, //ici on fait le choix d'inclure les event d'organisateurs qui n'ont pas de score
+			]
+		}
+	}
+
+	if(nb_places_wanted !== undefined) info.having[Op.and].push(   {'nb_places_left': {[Op.gte]: nb_places_wanted}}                                                                                    );
+	if(category !== undefined) info.having[Op.and].push(           this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('category')), Op.like, `%${category.toLowerCase()}%`)            );
+	if(range_places !== undefined) info.having[Op.and].push(       { participants_number: {[Op.between]: [range_places.min, range_places.max]} }                                                       );
+	if(address !== undefined) info.having[Op.and].push(            this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('address')), Op.like, `%${address.toLowerCase()}%`)              );
+	if(description !== undefined) info.having[Op.and].push(        this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('description')), Op.like, `%${description.toLowerCase()}%`)      );
+	if(event_name !== undefined) info.having[Op.and].push(         this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('name')), Op.like, `%${event_name.toLowerCase()}%`)              );
+	if(username !== undefined) info.having[Op.and].push(           this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('organizer.username')), Op.like, `%${username.toLowerCase()}%`)  );
+	if(range_date !== undefined) info.having[Op.and].push(         { date : {[Op.lte]: range_date.max, [Op.gte]: range_date.min} }                                                                     );
+	if(score_host_min !== undefined) info.having[Op.and].push(     { 'organizer.score_host': {[Op.or]: {[Op.gte]: score_host_min, [Op.eq]: null} } }                                                   ); //ici on fait le choix d'inclure les event d'organisateurs qui n'ont pas de score
+
+	return this.sequelize.transaction(t => {
+		return Event.findAll(info);
+	});
+
+}
+
+
+//SQLite
+// SELECT Events.id, Events.participants_number, Events.category, Events.address, Events.description, Events.image_url, Events.name, Events.organizerId, count(*) as nb_participants, Events.participants_number - count(*) as nb_places_left, USers.score_host, Users.score_participant, Users.username, Users.email
+// FROM Events
+// JOIN EventParticipants ON Events.id = EventParticipants.EventId JOIN Users ON Events.organizerId = Users.id
+// GROUP BY Events.id
+// HAVING nb_places_left>42
+//     AND Events.category LIKE '%Randonne%'
+//     AND Events.participants_number BETWEEN 0 AND 60
+//     AND Events.description LIKE '%rando%'
+//     AND Events.name LIKE '%Luminy%'
+//     AND (Events.address LIKE '%zz%' OR Events.address LIKE '%adresse%')
+//     AND Users.username LIKE '%paul%';
+
+
+// having: {
+// 	[Op.and]: [
+// 	  this.sequelize.where(this.sequelize.fn('LOWER', this.sequelize.col('category')), Op.like, `%${category}%`),
+// 	  { participants_number: {[Op.between]: [range_places.min, range_places.max]} },
+// 	  { description: {[Op.like]: `%${description}%`} },
+// 	  { name: {[Op.like]: `%${event_name}%`} },
+// 	  { address: {[Op.like]: `%${address}%`} },
+// 	  { 'organizer.username': {[Op.like]: `%${username}%`} },
+// 	  { 'nb_places_left': {[Op.gte]: nb_places_wanted} },
+// 	  { date : {
+// 		[Op.lte]: range_date.max,
+// 		[Op.gte]: range_date.min,
+// 	  }},
+// 	]}
+// });
+
 
 
 }
