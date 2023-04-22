ffd3e617d4df00ee3dafdf23c8bbe5fbf67b9348
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-04-23 00:36:48 +0200

    ajout en commentaires de commandes pratiques

--- script/start_mysql.sh
+++ script/start_mysql.sh
@@ -17,3 +17,16 @@ brew services start mysql
 
 # DEFAULT LOGIN
 # usernam:root password:empty
+
+
+# Creer la database de test:
+# CREATE DATABASE IF NOT EXISTS sn_test_db;
+
+# Creer la database de production:
+# CREATE DATABASE IF NOT EXISTS sn_prod_db;
+
+# Pour query la db avec mysql en ligne de commande:
+# mysql -u root
+# mysql> connect sn_test_db;
+# mysql> show tables;
+# mysql> select * from users;
