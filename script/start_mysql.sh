7c40e8ae7cea8e6752c7c21ec2498c2e0f7b3f1b
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-04-23 00:35:34 +0200

    script start mysql server

---
+++ script/start_mysql.sh
@@ -0,0 +1,19 @@
+# Installer MySQL si nécessaire 
+## brew insall mysql
+
+# Démarrer le service MySQL
+# https://stackoverflow.com/questions/100948/how-do-you-stop-mysql-on-a-mac-os-install
+brew services start mysql
+
+# Arrêter le service MySQL
+## brew services stop mysql
+
+# Redémarrer le service MySQL
+## brew services restart mysql
+
+
+# Créer une base de données
+# https://www.digitalocean.com/community/tutorials/how-to-use-sequelize-with-node-js-and-mysql
+
+# DEFAULT LOGIN
+# usernam:root password:empty
