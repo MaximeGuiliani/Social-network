0d0ce7e86d2223a55c1b82599e0f8eb94b1feaf4
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-04-23 00:36:09 +0200

    migration vers mySQL

--- database/setup_db.js
+++ database/setup_db.js
@@ -6,16 +6,34 @@ function connect_db(context){
   Sequelize.useCLS(cls.createNamespace('project_ns'));
   
   if(context=="prod"){
-    return new Sequelize({
-      dialect: "sqlite",
-      storage: "./database/database_prod.sqlite",
-    }); 
+    return new Sequelize('sn_prod_db','root', '',{
+      host: 'localhost',
+      dialect: 'mysql',
+      collate: 'utf8mb4_unicode_ci',
+      charset: 'utf8mb4',
+      dialectOptions: {
+        charset: 'utf8mb4',
+      },
+      define: {
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_unicode_ci'
+      }
+    });
   }
 
-  return new Sequelize({
-    dialect: "sqlite",
-    storage: "./database/database_test.sqlite",
-  }); 
+  return new Sequelize('sn_test_db','root', '',{
+    host: 'localhost',
+    dialect: 'mysql',
+    collate: 'utf8mb4_unicode_ci',
+    charset: 'utf8mb4',
+    dialectOptions: {
+      charset: 'utf8mb4',
+    },
+    define: {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_unicode_ci'
+    }
+  });
 
 }
 
@@ -35,6 +53,11 @@ export async function setup_db(force, context) {
   let sequelize;
   try {
     sequelize = connect_db(context)
+
+    // disable only_full_group_by in MySQL or Sequelize, utile pour la focntion de recherche d'event
+    // https://stackoverflow.com/questions/35882816/how-to-disable-only-full-group-by-in-mysql-or-sequelize
+    await sequelize.query('SET sql_mode = ""')
+
     User.init(sequelize);
     Event.init(sequelize);
     await sequelize.authenticate();
