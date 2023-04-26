dcc5f424e534bfa2005196fd3ad0fb6e7b9842b2
maximeGuiliani <maxime.guiliani@hotmail.fr>
2023-04-26 16:30:40 +0200

    gestion des routes

--- api/routes/events.js
+++ api/routes/events.js
@@ -1,53 +1,72 @@
 import { Router } from "express";
+import { Event } from "../../database/models/Event.js";
 const router = Router();
+import {schemaCreateEvent} from "../validator/validatorsEvents.js"; 
 
-router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests to /events'
-    });
+
+router.get("/", (req, res, next) => {
+  // TODO recupérer tous les events de la base de données
+  const events = [new Event(), new Event(), new Event()];
+  res.status(200).json({
+    message: "Handling GET requests to /events : returning all events",
+    events: events,
+  });
 });
 
-router.post('/', (req, res, next) => {
-    const event = {
-        name: req.body.name,
-        description: req.body.description
-
-    };
-    res.status(201).json({
-        message: 'Handling POST requests to /events',
-        createdEvent: event
+router.post("/create", (req, res, next) => {
+  // TODO : vérifier que l'utilisateur est connecté
+  // TODO : prendre l'id de la personne connecté si possible
+  // TODO : validation des données
+  // TODO : ajouter l'event a la BD si tout est valide
+  
+  const result = schemaCreateEvent.validate(req.body)
+  if (result.error) {
+    res.status(400).json({
+      message: "Bad request",
+      error: result.error,
     });
+    return;
+  }
+
+  const validatedEvent = new Event();
+
+  res.status(201).json({
+    message: "Handling POST requests to /events/create",
+    createdEvent: validatedEvent,
+  });
 });
 
-router.get('/:eventId', (req, res, next) => {
-    const id = req.params.eventId;
-    if (id === 'special') {
-        res.status(200).json({
-            message: 'You discovered the special ID',
-            id: id
-        });
-    } else {
-        res.status(200).json({
-            message: 'You passed an ID'
-        });
-    }
-
-
+router.get("/:eventId", (req, res, next) => {
+  const id = req.params.eventId;
+  // TODO : vérifier que l'id est bien un nombre
+  // TODO : vérifier que l'id existe dans la base de données
+  // TODO : récupérer l'event de la base de données
+  const event = new Event();
+  res.status(200).json({
+    message: "Handling GET requests to /events/" + req.params.eventId,
+    event: event,
+  });
 });
 
+router.patch("/:eventId", (req, res, next) => {
+  // TODO : validation des données
+  // TODO : vérifier que eventID est un ID
 
+  const modifiedEvent = new Event();
 
-router.patch('/:eventId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated event!'
-    });
-
+  res.status(200).json({
+    message: "Updated event with id " + req.params.eventId,
+    modifiedEvent: modifiedEvent,
+  });
 });
 
-router.delete('/:eventId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted event!'
-    });
+router.delete("/:eventId", (req, res, next) => {
+  // TODO : vérifier que eventID est un ID
+  // TODO : verifier que l'event existe dans la base de données
+  // TODO : supprimer l'event de la base de données si tout est valide et que l'utilisateur est l'organisateur de l'event
+  res.status(200).json({
+    message: "Deleted event with id : " + req.params.eventId,
+  });
 });
 
 export default router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,26 +1,51 @@
 import { Router } from "express";
-import { createUser, getAllUsers, updateUser, deleteUser } from "./../../db/users.js";
-
+import {User} from "./../../database/models/User.js"
 const router = Router();
 
-router.post("/", async (req, res, next) => {
-  const results = await createUser(req.body);
-  res.status(201).json({ id: results[0] });
+// TODO validation des données
+// TODO Faire des catch pour les erreurs de la base de données
+
+router.post("/create", async (req, res, next) => {
+
+  if(req.body.username == null || req.body.email == null || req.body.password_hash == null){
+    res.status(400).json({ status: "Missing parameters." });
+    return;
+  }
+    // TODO verifier que le username n'apparait pas dans la base
+    // TODO verifier que l'email est bien un email valide et qu'il n'est pas déjà utilisé
+
+  const new_user = User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password_hash: req.body.password_hash,
+  });
+  res.status(201).json({ status: "User added sucessfully.", AddedUser: new_user });
 });
 
 router.get("/", async (req, res, next) => {
-  const users = await getAllUsers();
+  const users = await (await myDAO).get_all_users();
   res.status(200).json(users);
 });
 
-router.patch("/:userId", async (req, res, next) => {
-  const user = await updateUser(req.params.userId, req.body);
-  res.status(200).json({ user });
+
+
+router.get("/:userName", async (req, res, next) => {
+  const user = await (await myDAO).get_user_by_username(req.params.userName);
+  res.status(200).json(user);
 });
 
-router.delete("/:userId", async (req, res, next) => {
-  await deleteUser(req.params.userId);
-  res.status(200).json({ sucess: true });
+router.patch("/:userName", async (req, res, next) => {
+  const user = await (await myDAO).update_user(
+    req.params.userName
+  );
+  res.status(200).json({deletedUser : user});
 });
+
+router.delete("/:userName", async (req, res, next) => {
+  const user = await (await myDAO).remove_user_by_username(req.params.userName);
+  res.status(200).json({deletedUser : user});
+});
+
+
 
 export default router;
