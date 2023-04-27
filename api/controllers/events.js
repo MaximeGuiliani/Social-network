f4f32d5bab27ede61d621becf63d30dd21150778
maximeGuiliani <maxime.guiliani@hotmail.fr>
2023-04-27 20:04:40 +0200

    refactor routes + ajout controller pur events

---
+++ api/controllers/events.js
@@ -0,0 +1,153 @@
+import { Event } from "../../database/models/Event.js";
+import {
+  validate,
+  schemaCreateEvent,
+  schemaId,
+  schemaUpdateEvent,
+} from "../validator/validatorsEvents.js";
+import { myDAO } from "../../app.js";
+
+export async function event_get_all(req, res, next) {
+  (await myDAO)
+    .get_all_events()
+    .then(function (result) {
+      res.status(200).json({
+        message: "Handling GET requests to /events : returning all events",
+        events: result,
+      });
+    })
+    .catch(function (err) {
+      res.status(400).json({
+        message: "Bad request",
+        error: err,
+      });
+    });
+}
+
+
+export async function event_create(req, res, next) {
+  req.body.organizerId = req.userData.id;
+  const validatedEvent = validate(schemaCreateEvent.validate(req.body), res);
+  if (validatedEvent == null) {
+    return;
+  }
+  const event = new Event(validatedEvent);
+  await (
+    await myDAO
+  )
+    .save_event(event)
+    .then(function (result) {
+      res.status(201).json({
+        message: "Handling POST requests to /events/create",
+        createdEvent: event,
+      });
+    })
+    .catch(function (err) {
+      res.status(400).json({
+        message: "Bad request",
+        error: err,
+      });
+    });
+}
+
+export async function event_get_by_id(req, res, next) {
+  const validId = validate(schemaId.validate(req.params.eventId), res);
+  if (validId == null) {
+    return;
+  }
+  (await myDAO)
+    .get_event_by_id(validId)
+    .then(function (event) {
+      if (event == null) {
+        res.status(404).json({
+          message: "Event not found",
+        });
+        return;
+      }
+      res.status(200).json({
+        message: "Handling GET requests to /events/" + req.params.eventId,
+        event: event,
+      });
+    })
+    .catch(function (err) {
+      res.status(400).json({
+        message: "Bad request",
+        error: err,
+      });
+    });
+}
+
+export async function event_update(req, res, next) {
+  const validId = validate(schemaId.validate(req.params.eventId), res);
+  if (validId == null) {
+    return;
+  }
+
+  if (req.userData.id != (await myDAO).get_event_by_id(validId).organizerId) {
+    res.status(401).json({
+      message: "Unauthorized access",
+    });
+    return;
+  }
+
+  const validUpdate = validate(schemaUpdateEvent.validate(req.body), res);
+  if (validUpdate == null) {
+    return;
+  }
+
+  (await myDAO)
+    .update_event_by_id(
+      validId,
+      validUpdate.name,
+      validUpdate.category,
+      validUpdate.address,
+      validUpdate.description,
+      validUpdate.image_url
+    )
+    .then(function (modifiedEvent) {
+      res.status(200).json({
+        message: "Updated event with id " + validId,
+        modifiedEvent: validUpdate,
+      });
+    })
+    .catch(function (err) {
+      res.status(400).json({
+        message: "Bad request",
+        error: err,
+      });
+    });
+}
+
+export async function event_delete(req, res, next) {
+  const validId = validate(schemaId.validate(req.params.eventId), res);
+  if (validId == null) {
+    return;
+  }
+
+  if (req.userData.id != (await myDAO).get_event_by_id(validId).organizerId) {
+    res.status(401).json({
+      message: "Unauthorized access",
+    });
+    return;
+  }
+
+  (await myDAO)
+    .remove_event_by_id(validId)
+    .then(function (result) {
+      if (result == 0) {
+        res.status(404).json({
+          message: "Event not found",
+        });
+        return;
+      }
+      res.status(200).json({
+        message: "Deleted event with id : " + validId,
+      });
+    })
+    .catch(function (err) {
+      res.status(400).json({
+        message: "Bad request",
+        error: err,
+      });
+    });
+}
--- api/routes/events.js
+++ api/routes/events.js
@@ -1,170 +1,27 @@
 import { Router } from "express";
-import { Event } from "../../database/models/Event.js";
 const router = Router();
-import {
-  validate,
-  schemaCreateEvent,
-  schemaId,
-  schemaUpdateEvent,
-} from "../validator/validatorsEvents.js";
 import checkAuth from "../middleware/check-auth.js";
-import { myDAO } from "../../app.js";
+import { event_create, event_delete, event_get_all, event_get_by_id, event_update } from "../controllers/events.js";
 
 
-// (GET) /users
+// (GET) /events
 
-router.get("/", async (req, res, next) => {
-  (await myDAO)
-    .get_all_events()
-    .then(function (result) {
-      res.status(200).json({
-        message: "Handling GET requests to /events : returning all events",
-        events: result,
-      });
-    })
-    .catch(function (err) {
-      res.status(400).json({
-        message: "Bad request",
-        error: err,
-      });
-    });
-});
+router.get("/",event_get_all );
 
-// (POST) /users/create
+// (POST) /events/create
 
-router.post("/create", checkAuth, async (req, res, next) => {
-  req.body.organizerId = req.userData.id;
-  const validatedEvent = validate(schemaCreateEvent.validate(req.body), res);
-  if (validatedEvent == null) {
-    return;
-  }
-  const event = new Event(validatedEvent);
-  await (
-    await myDAO
-  )
-    .save_event(event)
-    .then(function (result) {
-      res.status(201).json({
-        message: "Handling POST requests to /events/create",
-        createdEvent: event,
-      });
-    })
-    .catch(function (err) {
-      res.status(400).json({
-        message: "Bad request",
-        error: err,
-      });
-    });
-});
+router.post("/create", checkAuth, event_create);
 
-// (GET) /users/:userName
+// (GET) /events/:eventId
 
-router.get("/:eventId", async (req, res, next) => {
-  const validId = validate(schemaId.validate(req.params.eventId), res);
-  if (validId == null) {
-    return;
-  }
-  (await myDAO)
-    .get_event_by_id(validId)
-    .then(function (event) {
-      if (event == null) {
-        res.status(404).json({
-          message: "Event not found",
-        });
-        return;
-      }
-      res.status(200).json({
-        message: "Handling GET requests to /events/" + req.params.eventId,
-        event: event,
-      });
-    })
-    .catch(function (err) {
-      res.status(400).json({
-        message: "Bad request",
-        error: err,
-      });
-    });
-});
+router.get("/:eventId", event_get_by_id);
 
-// (PATCH) /users/:userName
+// (PATCH)  /events/:eventId
 
-router.patch("/:eventId",checkAuth, async (req, res, next) => {
-  
-  const validId = validate(schemaId.validate(req.params.eventId), res);
-  if (validId == null) {
-    return;
-  }
-  
-  if( req.userData.id != (await myDAO).get_event_by_id(validId).organizerId){
-    res.status(401).json({
-      message: "Unauthorized access",
-    });
-    return;
-  }
-  
-  
-  const validUpdate = validate(schemaUpdateEvent.validate(req.body), res);
-  if (validUpdate == null) {
-    return;
-  }
+router.patch("/:eventId",checkAuth, event_update);
 
-  (await myDAO)
-    .update_event_by_id(
-      validId,
-      validUpdate.name,
-      validUpdate.category,
-      validUpdate.address,
-      validUpdate.description,
-      validUpdate.image_url
-    )
-    .then(function (modifiedEvent) {
-      res.status(200).json({
-        message: "Updated event with id " + validId,
-        modifiedEvent: validUpdate,
-      });
-    })
-    .catch(function (err) {
-      res.status(400).json({
-        message: "Bad request",
-        error: err,
-      });
-    });
-});
+// (DELETE) /events/:eventId
 
-// (DELETE) /users/:userName
-
-router.delete("/:eventId", async (req, res, next) => {
-  const validId = validate(schemaId.validate(req.params.eventId), res);
-  if (validId == null) {
-    return;
-  }
-
-  if( req.userData.id != (await myDAO).get_event_by_id(validId).organizerId){
-    res.status(401).json({
-      message: "Unauthorized access",
-    });
-    return;
-  }
-
-  (await myDAO)
-    .remove_event_by_id(validId)
-    .then(function (result) {
-    if(result==0){
-        res.status(404).json({
-            message: "Event not found",
-          });
-          return;
-    }
-      res.status(200).json({
-        message: "Deleted event with id : " + validId,
-      });
-    })
-    .catch(function (err) {
-      res.status(400).json({
-        message: "Bad request",
-        error: err,
-      });
-    });
-});
+router.delete("/:eventId",checkAuth ,event_delete);
 
 export default router;
