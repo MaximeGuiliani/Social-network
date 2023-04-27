45f373d96a59632ad4fd1c69a2f6418278e51e52
maximeGuiliani <maxime.guiliani@hotmail.fr>
2023-04-27 16:32:05 +0200

    schemaUpdateUser + schemaUsername

--- api/validator/validatorsUsers.js
+++ api/validator/validatorsUsers.js
@@ -6,12 +6,17 @@ const schemacreateUser = Joi.object().keys({
   email: Joi.string().email().required(),
 });
 
+const schemaUpdateUser = Joi.object().keys({
+  username: Joi.string().alphanum().min(3).max(30).required(),
+  password_hash: Joi.string().pattern(new RegExp("^[0-9a-f]{64,}$")).required(),
+  email: Joi.string().email().required(),
+});
+
 const schemaUpdatePassword = Joi.object().keys({
   oldpassword: Joi.string().alphanum().min(3).max(30).required(),
   newpassword: Joi.string().pattern(new RegExp("^[0-9a-f]{64,}$")),
-  confirmpassword:Joi.string().valid(Joi.ref('newpassword')).required()
+  confirmpassword: Joi.string().valid(Joi.ref("newpassword")).required(),
 });
-
 
 function validate(validation, res) {
   if (validation.error) {
@@ -24,4 +29,21 @@ function validate(validation, res) {
   return validation.value;
 }
 
-export { schemaUpdatePassword, schemacreateUser, validate };
+const schemaUsername = Joi.string()
+  .regex(/^[a-zA-Z0-9_]+$/)
+  .required()
+  .messages({
+    "string.base": "Username should be a string",
+    "string.empty": "Username cannot be empty",
+    "string.pattern.base":
+      "Username can only contain letters, numbers, and underscores",
+    "any.required": "Username is required",
+  });
+
+export {
+  schemaUpdatePassword,
+  schemacreateUser,
+  validate,
+  schemaUsername,
+  schemaUpdateUser,
+};
