bf1535470895f9b4610a3abcc0538e47e2f91df5
maximeGuiliani <maxime.guiliani@hotmail.fr>
2023-04-27 13:06:33 +0200

    POST /users/create

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,25 +1,41 @@
 import { Router } from "express";
-import {User} from "./../../database/models/User.js"
+import { User } from "./../../database/models/User.js";
 const router = Router();
+import { schemacreateUser } from "../validator/validatorsUsers.js";
+import { myDAO } from "../../app.js";
 
 // TODO validation des données
 // TODO Faire des catch pour les erreurs de la base de données
 
 router.post("/create", async (req, res, next) => {
+  // TODO verifier que le username n'apparait pas dans la base
+  // TODO verifier que l'email est bien un email valide et qu'il n'est pas déjà utilisé
 
-  if(req.body.username == null || req.body.email == null || req.body.password_hash == null){
-    res.status(400).json({ status: "Missing parameters." });
+  const validUser = validate(schemacreateUser.validate(req.body));
+
+  if (validUser == null) {
     return;
   }
-    // TODO verifier que le username n'apparait pas dans la base
-    // TODO verifier que l'email est bien un email valide et qu'il n'est pas déjà utilisé
-
-  const new_user = User.create({
-    username: req.body.username,
-    email: req.body.email,
-    password_hash: req.body.password_hash,
-  });
-  res.status(201).json({ status: "User added sucessfully.", AddedUser: new_user });
+  const validatedUser = new User(validUser);
+  await (
+    await myDAO
+  )
+    .add_user(
+      validatedUser.username,
+      validatedUser.email,
+      validatedUser.password_hash
+    )
+    .then(function (user) {
+      res
+        .status(201)
+        .json({ status: "User added sucessfully.", AddedUser: user });
+    })
+    .catch(function (err) {
+      res.status(400).json({
+        message: "Bad request",
+        error: err,
+      });
+    });
 });
 
 router.get("/", async (req, res, next) => {
@@ -27,25 +43,32 @@ router.get("/", async (req, res, next) => {
   res.status(200).json(users);
 });
 
-
-
 router.get("/:userName", async (req, res, next) => {
   const user = await (await myDAO).get_user_by_username(req.params.userName);
   res.status(200).json(user);
 });
 
 router.patch("/:userName", async (req, res, next) => {
-  const user = await (await myDAO).update_user(
-    req.params.userName
-  );
-  res.status(200).json({deletedUser : user});
+  const user = await (await myDAO).update_user(req.params.userName);
+  res.status(200).json({ deletedUser: user });
 });
 
 router.delete("/:userName", async (req, res, next) => {
   const user = await (await myDAO).remove_user_by_username(req.params.userName);
-  res.status(200).json({deletedUser : user});
+  res.status(200).json({ deletedUser: user });
 });
 
+// Part for validation ______________________
 
+function validate(validation, res) {
+  if (validation.error) {
+    res.status(400).json({
+      message: "Bad request",
+      error: validation.error,
+    });
+    return null;
+  }
+  return validation.value;
+}
 
 export default router;
