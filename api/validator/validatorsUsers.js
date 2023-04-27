31b14a6aee92bc1bfcfdf8e88d0d872af9efa0ef
maximeGuiliani <maxime.guiliani@hotmail.fr>
2023-04-27 13:06:52 +0200

    validation create user

--- api/validator/validatorsUsers.js
+++ api/validator/validatorsUsers.js
@@ -1,9 +1,9 @@
 import Joi from "joi";
 
-const schemaUpdateUser = Joi.object().keys({
+const schemacreateUser = Joi.object().keys({
   username: Joi.string().alphanum().min(3).max(30).required(),
-  password_hash: Joi.string().pattern(new RegExp("^[0-9a-f]{64,}$")),
-  email: Joi.string().email(),
+  password_hash: Joi.string().pattern(new RegExp("^[0-9a-f]{64,}$")).required(),
+  email: Joi.string().email().required(),
 });
 
 const schemaUpdatePassword = Joi.object().keys({
@@ -12,4 +12,4 @@ const schemaUpdatePassword = Joi.object().keys({
   confirmpassword:Joi.string().valid(Joi.ref('newpassword')).required()
 });
 
-export { schemaUpdatePassword, schemaUpdateUser };
+export { schemaUpdatePassword, schemacreateUser };
