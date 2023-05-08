e084baaa751618627e359b7db0c392bd8476318e
Léo-Paul MUSARDO <leo-paul.musardo@etu.univ-amu.fr>
2023-05-08 12:16:21 +0200

    add new Models

--- database/models/index.js
+++ database/models/index.js
@@ -1,4 +1,8 @@
 import { User } from "./User.js";
 import { Event } from "./Event.js";
+import { MainCategory } from "./MainCategory.js";
+import { Address } from "./Address.js";
+import { EventMessage } from "./EventMessage.js";
+import { Note } from "./Note.js";
 
-export { User, Event }
+export { User, Event, MainCategory, Address, EventMessage, Note }
