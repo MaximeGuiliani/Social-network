ca60cfcab01f3bddf97152d5430eea3c646aeb75
maximeGuiliani <maxime.guiliani@hotmail.fr>
2023-05-22 11:44:03 +0200

    test messages

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -2,16 +2,14 @@ import { Router } from "express";
 const router = Router();
 import checkAuth from "../middleware/check-auth.js";
 import {
-    add_messages_to_event,
-    get_all_messages_from_event
+  add_messages_to_event,
+  get_all_messages_from_event,
 } from "../controllers/messages.js";
 
-
 // (GET) /messages
-router.get("/", get_all_messages_from_event);
+router.get("/:eventId", get_all_messages_from_event);
 
 // (POST) /messages
 router.post("/", add_messages_to_event);
-
 
 export default router;
